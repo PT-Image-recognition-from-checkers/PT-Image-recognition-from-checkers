7919ad76a87436bbac7e0a612a3319f7bbc07702
hubertjankowski <hubertjan98@gmail.com>
2020-04-17 17:16:43 +0200

    Init

---
+++ Main.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+
+import cv2
+import numpy as np
+
+url = 'http://192.168.1.32:8080/shot.jpg'
+
+while True:
+    imgResp = urlopen(url)
+    imgNp=np.array(bytearray(imgResp.read()),dtype=np.uint8)
+    img=cv2.imdecode(imgNp,-1)
+    cv2.imshow('Webcam',img)
+    if ord('q')==cv2.waitKey(10):
+        exit(0)

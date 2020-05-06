69cfaa3d71bba9b6b98ecf3910a4a2f99eaf4e2d
M4T11 <m.galan777@gmail.com>
2020-05-06 14:21:18 +0200

    Adding the option of immediate freezing of the image at the moment when player move pawns

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import numpy as np
 import pygame
 import sys
 import virtual_board
+import time
 
 #telefon
-url = 'http://192.168.2.8:8080/shot.jpg'
+# url = 'http://192.168.2.8:8080/shot.jpg'
 
 #laptop
-#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 pygame.init()  # essential for pygame
 pygame.font.init()  # for text
@@ -27,20 +28,58 @@ wq = pygame.image.load("assets/white_queen.png")
 # necessary for capping the game at 60FPS
 clock = pygame.time.Clock()
 
+values = None
+global img, dst
 while True:
     #telefon
-    imgResp = urlopen(url)
-    imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
-    img = cv2.imdecode(imgNp, 1)
+    # imgResp = urlopen(url)
+    # imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
+    # img = cv2.imdecode(imgNp, 1)
 
     # laptop
-    # ret, img = cap.read()
+    ret, img = cap.read()
+    dst, crop_rect = board.find_edges_and_perspective_transform(img)
 
-    dst = board.find_edges_and_perspective_transform(img)
+    # print("PRINT", np.array(list(crop_rect)))
+
+    if values is None:
+        values = np.array(list(crop_rect), dtype='float32')
+        values1 = values + 30
+        values2 = values - 30
+
+    comparison1 = crop_rect < values1
+    comparison2 = crop_rect > values2
+    equal_arrays = comparison1.all()
+    equal_arrays1 = comparison2.all()
+
+    while not equal_arrays and not equal_arrays1:
+        print("PRAWDA")
+        print(equal_arrays)
+        print(equal_arrays1)
+        time.sleep(1)
+        # KOMPUTER
+        ret, img = cap.read()
+        # TELEFON
+        # imgResp = urlopen(url)
+        # imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
+        # img = cv2.imdecode(imgNp, 1)
+        dst, crop_rect = board.find_edges_and_perspective_transform(img)
+        comparison1 = crop_rect < values1
+        comparison2 = crop_rect > values2
+        equal_arrays = comparison1.all()
+        equal_arrays1 = comparison2.all()
+        # print(checkers_list)
+
+
+    cv2.imshow("dst", dst)
+    # if checkers_list is not None:
+    #     checkers_list_n = checkers_list
+    # print(checkers_list_n)
     checkers_list = board.find_checkers(dst)
     print(checkers_list)
 
     dst_RGB = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+    cv2.imshow("dst_RGB", dst_RGB)
     dst_RGB = np.rot90(dst_RGB)
     dst_RGB = pygame.surfarray.make_surface(dst_RGB)
     dst_RGB = pygame.transform.flip(dst_RGB, True, False)
@@ -54,24 +93,26 @@ while True:
 
     position = 0
     checkers = virtual_board.find_fields()
-    for ch in checkers_list:
-        if ch == 'RP':
-            screen.blit(rp, (checkers[position][0][0], checkers[position][0][1]))
-            position += 1
-        elif ch == 'WP':
-            screen.blit(wp, (checkers[position][0][0], checkers[position][0][1]))
-            position += 1
-        elif ch == 'WQ':
-            screen.blit(wq, (checkers[position][0][0], checkers[position][0][1]))
-            position += 1
-        elif ch == 'RQ':
-            screen.blit(rq, (checkers[position][0][0], checkers[position][0][1]))
-            position += 1
-        else:
-            position += 1
+    if checkers_list is not None:
+        for ch in checkers_list:
+            if ch == 'RP':
+                screen.blit(rp, (checkers[position][0][0], checkers[position][0][1]))
+                position += 1
+            elif ch == 'WP':
+                screen.blit(wp, (checkers[position][0][0], checkers[position][0][1]))
+                position += 1
+            elif ch == 'WQ':
+                screen.blit(wq, (checkers[position][0][0], checkers[position][0][1]))
+                position += 1
+            elif ch == 'RQ':
+                screen.blit(rq, (checkers[position][0][0], checkers[position][0][1]))
+                position += 1
+            else:
+                position += 1
+
 
     pygame.display.update()
-    clock.tick(60)
+    # clock.tick(60)
 
     if ord('q') == cv2.waitKey(10):
         exit(0)

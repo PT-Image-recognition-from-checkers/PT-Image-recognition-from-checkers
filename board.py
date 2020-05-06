202d3acd21e54a39ccf5951df08099123deffa2b
Tarnacki <tarnac97@gmail.com>
2020-05-06 14:15:11 +0200

    change of perspective transformation

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+import operator
+
 import numpy as np
 import cv2
 
@@ -14,41 +16,41 @@ u_white = np.array([180, 50, 255])
 l_yellow = np.array([22, 93, 0])
 u_yellow = np.array([45, 255, 255])
 
-l_blue = np.array([110, 50, 50])
-u_blue = np.array([130, 255, 255])
+l_blue = np.array([94, 80, 2])
+u_blue = np.array([126, 255, 255])
+
 
 
 def find_edges_and_perspective_transform(image):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, l_green, u_green)
+    imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY)
+#    cv2.imshow("thresh", thresh)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+#    print(len(contours))
+    cv2.drawContours(image, contours, 1, (0, 255, 0), 3)
+#    cv2.imshow("test", image)
+    polygon = contours[1]
+#    print(polygon)
+    bottom_right, _ = max(enumerate([pt[0][0] + pt[0][1] for pt in
+                                     polygon]), key=operator.itemgetter(1))
+    top_left, _ = min(enumerate([pt[0][0] + pt[0][1] for pt in
+                                 polygon]), key=operator.itemgetter(1))
+    bottom_left, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in
+                                    polygon]), key=operator.itemgetter(1))
+    top_right, _ = max(enumerate([pt[0][0] - pt[0][1] for pt in
+                                  polygon]), key=operator.itemgetter(1))
 
-    kernel_er = np.ones((6, 6), np.uint8)
-    kernel_dil = np.ones((30, 30), np.uint8)
-    erosion = cv2.erode(mask, kernel_er, iterations=1)
-    dilation = cv2.dilate(erosion, kernel_dil, iterations=1)
+    crop_rect = [polygon[top_left][0], polygon[top_right][0], polygon[bottom_right][0], polygon[bottom_left][0]]
 
-    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(hsv, contours, -1, (128, 255, 187), 3)
+    top_left, top_right, bottom_right, bottom_left = crop_rect[0], crop_rect[1], crop_rect[2], crop_rect[3]
 
-    list_of_edges_points = []
-
-    for i in contours:
-        cnt = i
-        M = cv2.moments(cnt)
-        cx = int(M['m10'] / M['m00'])
-        cy = int(M['m01'] / M['m00'])
-        hsv = cv2.line(hsv, (cx, cy), (cx, cy), (128, 255, 187), 5)
-        list_of_edges_points.append([cx, cy])
-
-    if (len(list_of_edges_points) == 4):
-        pts1 = np.float32([list_of_edges_points[3], list_of_edges_points[2], list_of_edges_points[1], list_of_edges_points[0]])
-        pts2 = np.float32([[0, 0], [600, 0], [0, 600], [600, 600]])
-        M = cv2.getPerspectiveTransform(pts1, pts2)
-        dst = cv2.warpPerspective(image, M, (600, 600))
-
-        return dst
-    else:
-        return image
+    dst = np.array([top_left, top_right, bottom_left, bottom_right], dtype='float32')
+    pts2 = np.float32([[0, 0], [600, 0], [0, 600], [600, 600]])
+    m = cv2.getPerspectiveTransform(dst, pts2)
+    image2 = cv2.warpPerspective(image, m, (600, 600))
+#    cv2.imshow("image", image2)
+    return image2, crop_rect
 
 
 def find_checkers(image):
@@ -58,9 +60,13 @@ def find_checkers(image):
 
     #Maski dla kolorów
     mask_w = cv2.inRange(hsv, l_white, u_white)
+#    cv2.imshow("white", mask_w)
     mask_r = cv2.inRange(hsv, l_red, u_red)
+#    cv2.imshow("red", mask_r)
     mask_y = cv2.inRange(hsv, l_yellow, u_yellow)
+#    cv2.imshow("yellow", mask_y)
     mask_b = cv2.inRange(hsv, l_blue, u_blue)
+#    cv2.imshow("blue", mask_b)
 
     median = cv2.medianBlur(image, 5)
     gray = cv2.cvtColor(median, cv2.COLOR_BGR2GRAY)
@@ -102,12 +108,12 @@ def find_checkers(image):
             # Sprawdzanie kolorów pionków
             if (cv2.countNonZero(area_w) / (height_w * width_w)) > 0.8:
                 color = 'WP'
-            elif (cv2.countNonZero(area_r) / (height_r * width_r)) > 0.8:
-                color = 'RP'
+            elif (cv2.countNonZero(area_y) / (height_y * width_y)) > 0.8:
+                color = 'WQ'
             elif (cv2.countNonZero(area_b) / (height_b * width_b)) > 0.8:
                 color = 'RQ'
             else:
-                color = 'WQ'
+                color = 'RP'
 
 
             # Ustawienie pionków na planszy
